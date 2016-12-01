f5dc7b99e7b31839168d62f05bac864bd49256ea
wmin0 <wmin0@hotmail.com>
2016-12-01 21:03:24 +0800

    2. hello

---
+++ src/hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div style={{color: 'red'}}>owo</div>
+    );
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,9 @@
-console.error('owo');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './hello';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let div = document.createElement('div');
+  document.body.appendChild(div);
+  ReactDOM.render(<Hello />, div);
+});
